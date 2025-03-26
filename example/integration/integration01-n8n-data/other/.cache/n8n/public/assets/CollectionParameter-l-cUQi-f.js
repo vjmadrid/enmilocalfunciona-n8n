1da4c6794f7050212497995e2177f224837c9707
vjmadrid <vjmadrid@MacBook-Pro-de-Mac-Admin.local>
2025-03-26 16:56:54 +0100

    Prepare mcp

--- example/integration/integration01-n8n-data/other/.cache/n8n/public/assets/CollectionParameter-l-cUQi-f.js
+++
@@ -1,4 +0,0 @@
-import { gx as _sfc_main } from "./index-DkwrpQEB.js";
-export {
-  _sfc_main as default
-};
--- example/integration/integration01-n8n-data/other/.cache/n8n/public/assets/global-link-actions-D2_SnpTc.js
+++
@@ -1,5 +0,0 @@
-import { D as createEventBus } from "./index-DkwrpQEB.js";
-const globalLinkActionsEventBus = createEventBus();
-export {
-  globalLinkActionsEventBus as g
-};
